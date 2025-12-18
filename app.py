27a60059b25686a86deda02763eace73cf3ed450
Harsh2023UCI8026 <143885470+Harsh2023UCI8026@users.noreply.github.com>
2025-12-18 05:34:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ if uploaded_file is not None:
 
         with col2:
             st.header("Most Busy Month")
-            busy_month=helper.month_activity_map(selected_user,df)
+            busy_month=helper.monthly_activity_map(selected_user,df)
             fig,ax=plt.subplots()
             ax.bar(busy_month.index, busy_month.values, color='pink')
             plt.xticks(rotation=45)
@@ -115,7 +115,7 @@ if uploaded_file is not None:
         st.title("Weekly Activity Map")
         user_heatmap=helper.activity_heatmap(selected_user,df)
         fig, ax = plt.subplots()
-        ax=sns.heatmap(user_heatmap)
+        ax =sns.heatmap(user_heatmap)
         st.pyplot(fig)
 
 
